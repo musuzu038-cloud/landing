2671093f230d1b019e768e69d5abadd3f76544a9
musuzu038-cloud <musuzu038@gmail.com>
2026-02-06 13:14:32 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,24 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+   preview: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    allowedHosts: [
+      'tochka-sborki.site',
+      'www.tochka-sborki.site',
+      'localhost',
+      '127.0.0.1'
+    ]
+  },
   server: {
     host: "::",
     port: 8080,
     hmr: {
       overlay: false,
     },
+  
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
