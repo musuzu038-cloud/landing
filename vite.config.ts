71e17dd8c10def7d393456966c5b5ba92dce17be
musuzu038-cloud <musuzu038@gmail.com>
2026-02-06 16:56:34 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,11 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-   preview: {
+  server: {
+    host: "::",
+    port: 8080,
+     },
+    preview: {
     host: true,
     port: 5173,
     strictPort: true,
@@ -15,14 +19,6 @@ export default defineConfig(({ mode }) => ({
       'localhost',
       '127.0.0.1'
     ]
-  },
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
